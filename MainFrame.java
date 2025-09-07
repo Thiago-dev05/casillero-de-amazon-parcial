ad34d8c8cc4250a6bfa02d300c895dfb627d9730
Angel <you@example.com>
2025-09-06 19:21:25 -0500

    Iniciar intefaz grafica

---
+++ MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public void initialize(){
+        this.setTitle("Gestión de Casilleros");
+        this.setSize(400, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    
+}
